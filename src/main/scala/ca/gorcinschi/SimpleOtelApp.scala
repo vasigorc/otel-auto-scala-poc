bb70d9593f6cebb2204b7697c1724f73c884d50f
Vasile Gorcinschi <vasigorc@gmail.com>
2022-04-11 12:26:26 -0400

    Paint touch SimpleOtelApp

--- src/main/scala/ca/gorcinschi/SimpleOtelApp.scala
+++ src/main/scala/ca/gorcinschi/SimpleOtelApp.scala
@@ -1,28 +1,27 @@
 package ca.gorcinschi
 
 import ca.gorcinschi.configuration.ConfigModule
+import ca.gorcinschi.helpers.UUIDGenerator
 import ca.gorcinschi.observability.ObservabilityModule
 import com.typesafe.scalalogging.LazyLogging
 import io.opentelemetry.api.common.AttributeKey.stringKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.metrics.Meter
 
-import java.util.UUID
 import scala.collection.immutable.LazyList.continually
 
-object SimpleOtelApp extends App with ObservabilityModule with ConfigModule with LazyLogging {
+object SimpleOtelApp extends App with ObservabilityModule with ConfigModule with LazyLogging with UUIDGenerator {
 
   val meter = opentelemetry.getMeter(config.getConfig.getString("observability.service-name"))
+  logger.info(s"Meter implementation is $meter")
 
   def recordBeat(meter: Meter, value: Long): Unit = {
-    val longCounterBuilder = meter
+    meter
       .counterBuilder("AppCounter")
-    logger.info(s"LongCounterBuilder implementation is $longCounterBuilder")
-    longCounterBuilder
       .setUnit("1")
       .setDescription("An example of counter")
       .buildWithCallback(measurement =>
-        measurement.record(value, Attributes.of(stringKey("some-attribute"), UUID.randomUUID().toString))
+        measurement.record(value, Attributes.of(stringKey("some-attribute"), stringifiedUUID()))
       )
   }
 
